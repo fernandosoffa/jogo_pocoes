accc05a07c15d0edfe7cd8495be58fbe9eb7814d
Fernando <fernando.soffa@gmail.com>
2026-06-16 21:43:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function beberPocao(tipo){
+    if(tipo === 'vermelho'){
+        alert("Você recuperou sua vida!");
+    }
+    else if(tipo === 'azul'){
+        alert("Você restarou sua mana!");
+    }
+    else{
+        alert("Puts...era veneno! Fim de jogo.")
+    }
+}
